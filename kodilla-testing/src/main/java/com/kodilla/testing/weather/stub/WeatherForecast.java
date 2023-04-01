0d49c409333a5c380778e6b5840fb2d7619684d0
Krystian Brzykowski <krystianbrzyk@vp.pl>
2023-04-01 12:42:05 +0200

    Task 6.5

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
@@ -0,0 +1,51 @@
+package com.kodilla.testing.weather.stub;
+
+import java.util.*;
+import java.util.Collections;
+
+public class WeatherForecast {
+    private Temperatures temperatures;
+
+
+    public WeatherForecast(Temperatures temperatures) {
+        this.temperatures = temperatures;
+    }
+
+    public Map<String, Double> calculateForecast() {
+
+        Map<String, Double> resultMap = new HashMap<>();
+
+        for (Map.Entry<String, Double> temperature :
+                temperatures.getTemperatures().entrySet()) {
+
+            // adding 1 celsius degree to current value
+            // as a temporary weather forecast
+            resultMap.put(temperature.getKey(), temperature.getValue() + 1.0); // [1]
+        }
+        return resultMap;
+    }
+
+    public double averageTemperatureMethod(Map<String, Double> map){
+
+        double result;
+        double sum = 0;
+        for (Double value : map.values()) {
+            sum += value;
+        }
+        return result = sum/map.size();
+    }
+
+    public double medianTemperaturesMethod(Map<String, Double> map){
+
+        List<Double> sortedList = new ArrayList<>(map.values());
+        Collections.sort(sortedList);
+
+        double result;
+            if(sortedList.size() % 2 == 0){
+                result = ((sortedList.size()/2 - 1) + (sortedList.size()/2 + 1))/2;
+        }else {
+                result = sortedList.get(sortedList.size()/2);
+            }
+            return result;
+    }
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/weather/stub/mock/WeatherForecastTestSuite.java
@@ -0,0 +1,67 @@
+package com.kodilla.testing.weather.stub.mock;
+
+import com.kodilla.testing.weather.stub.Temperatures;
+import com.kodilla.testing.weather.stub.WeatherForecast;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import java.util.HashMap;
+import java.util.Map;
+import static org.mockito.Mockito.when;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class WeatherForecastTestSuite {
+    private static Map<String, Double> temperaturesMap = new HashMap<>();
+
+    @BeforeAll
+    public static void beforeAll() {
+        temperaturesMap.put("Rzeszow", 24.0);
+        temperaturesMap.put("Krakow", 25.0);
+        temperaturesMap.put("Wroclaw", 26.0);
+        temperaturesMap.put("Warszawa", 23.0);
+        temperaturesMap.put("Gdansk", 22.0);
+    }
+
+    @Mock
+    private Temperatures temperaturesMock;
+
+    @DisplayName("Test of a Weather Forecast")
+    @Test
+    void testCalculateForecastWithMock() {
+
+        //Given
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+
+        //When
+        int quantityOfSensors = weatherForecast.calculateForecast().size();
+
+        //Then
+        Assertions.assertEquals(5, quantityOfSensors);
+    }
+
+    @DisplayName("Test average temperature")
+    @Test
+    void testAverageTemperature(){
+        //Given
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+        //When
+        double average = weatherForecast.averageTemperatureMethod(temperaturesMap);
+        //Then
+        Assertions.assertEquals(24,average);
+    }
+
+    @DisplayName("Test median temperature")
+    @Test
+    void testMedianTemperature(){
+        //Given
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+        //When
+        double median = weatherForecast.medianTemperaturesMethod(temperaturesMap);
+        //Then
+        Assertions.assertEquals(24,median);
+    }
+}
