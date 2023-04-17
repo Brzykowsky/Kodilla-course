dc47fd6bf0488d3ec2809c2956080e50fd7ad1df
Krystian Brzykowski <krystianbrzyk@vp.pl>
2023-04-17 15:00:37 +0200

    Task task 8.4

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearcher.java
@@ -0,0 +1,13 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindingFlightClass {
+
+    public void findFilght(Flight flight){
+        Map<String,Boolean> airPortMAp = new HashMap<>();
+        
+    }
+
+}
