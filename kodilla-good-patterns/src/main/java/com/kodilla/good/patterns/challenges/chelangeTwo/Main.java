470a68bc0fe0b104f9bd847543bfd800c64f16e2
Krystian Brzykowski <krystianbrzyk@vp.pl>
2023-04-20 11:42:47 +0200

    Task task 9.2

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/chelangeTwo/Main.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/chelangeTwo/Main.java
@@ -1,11 +1,19 @@
 package com.kodilla.good.patterns.challenges.chelangeTwo;
 
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
 
         OrderService orderService = new OrderService();
         OrderRepository orderRepository = new OrderRepository();
 
+        User userKrystian = new User("Krystian", "Brzyk", "krystianbrzyk@vp.pl");
+        LocalDateTime timeOfOrder = LocalDateTime.of(2023, 4, 20, 14, 30, 0);
+        OrderRequest orderRequest = new OrderRequest(userKrystian, timeOfOrder,"Butter",2);
 
+        ProductOrderService productOrderService = new ProductOrderService(orderService, orderRepository);
+
+        productOrderService.process(orderRequest);
     }
 }
