95b971bc70eb9a356fa038ae68e5293805f5fede
Krystian Brzykowski <krystianbrzyk@vp.pl>
2023-04-25 12:28:36 +0200

    Task 10.2

--- kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
@@ -1,6 +1,5 @@
 package com.kodilla.spring.library;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,16 +7,21 @@ import java.util.List;
 @Service
 public final class Library {
 
-        private final List<String> books = new ArrayList<>();
+    private final List<String> books = new ArrayList<>();
+    private LibraryDbController libraryDbController;
 
-        @Autowired
-        private LibraryDbController libraryDbController;
-
-        public void saveToDb() {
-            libraryDbController.saveData();
-        }
-
-        public void loadFromDb() {
-            libraryDbController.loadData();
-        }
+    public Library(final LibraryDbController libraryDbController) {
+        this.libraryDbController = libraryDbController;
     }
+
+    public Library() {
+    }
+
+    public void saveToDb() {
+        libraryDbController.saveData();
+    }
+
+    public void loadFromDb() {
+        libraryDbController.loadData();
+    }
+}
