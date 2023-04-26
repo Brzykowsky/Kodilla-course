8aa6f02f35e43fd9ee52cac2f6c053981f34aea4
Krystian Brzykowski <krystianbrzyk@vp.pl>
2023-04-26 22:20:42 +0200

    Task 10.4

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -1,8 +1,10 @@
 package com.kodilla.spring.calculator;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope("prototype")
 public class Calculator {
 
     private final Display display;
--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Display.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Display.java
@@ -8,6 +8,4 @@ public class Display {
     public void displayValue(double val){
         System.out.println("Result: " + val);
     }
-
-
 }
