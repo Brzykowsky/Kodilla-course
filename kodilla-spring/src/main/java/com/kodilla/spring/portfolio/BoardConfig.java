9b511d8ae9a1a87d0f4c166b85b90df4973fac3f
Krystian Brzykowski <krystianbrzyk@vp.pl>
2023-04-26 12:31:09 +0200

    Task 10.3

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,2 +1,27 @@
-package com.kodilla.spring.portfolio;public class BoardConfig {
+package com.kodilla.spring.portfolio;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class BoardConfig {
+
+    @Bean(name = "Task to Do")
+    public TaskList toDoList(){
+        return new TaskList();
+    }
+
+    @Bean(name = "Task in Progress")
+    public TaskList inProgressList(){
+        return new TaskList();
+    }
+
+    @Bean(name = "Done Tasks")
+    public TaskList doneTasks(){
+        return new TaskList();
+    }
+
+    @Bean
+    public Board board(){
+        return new Board(toDoList(), inProgressList(),doneTasks());
+    }
 }
--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -1,2 +1,30 @@
-package com.kodilla.spring.portfolio;public class BoardTestSuite {
+package com.kodilla.spring.portfolio;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class BoardTestSuite {
+
+
+    @Test
+    public void testTaskAdd(){
+
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+        Board board = context.getBean(Board.class);
+        //When
+        board.getToDoList().getTasks().add("Task1");
+        board.getInProgressList().getTasks().add("Task2");
+        board.getDoneList().getTasks().add("Task2");
+        //Then
+        assertEquals(1,board.getToDoList().getTasks().size());
+        assertEquals(1,board.getInProgressList().getTasks().size());
+        assertEquals(1,board.getDoneList().getTasks().size());
+
+    }
 }
